a7a9fe51f98ce6827764ff0e4d2f6205f13563e2
Aline <aline.araujo0666@gmail.com>
2023-03-28 19:29:38 -0300

    Inversão de caracteres de uma String (Python)
    Projeto Target

---
+++ AlgoritmoInversaoCaracteres.py
@@ -0,0 +1,7 @@
+string_original = input("Digite uma string: ")
+
+# Invertendo a string utilizando fatiamento de strings
+
+string_invertida = string_original[::-1]
+print("String original:  ", string_original)
+print("String invertida: ", string_invertida)
